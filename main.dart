65c9ba669b1c6baa7d92b7da55bfea9a4a1aa022
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-04-06 13:47:52 +0500

    if else basic understanding

--- main.dart
+++ main.dart
@@ -1,4 +1,9 @@
 void main(){
-  print("hello");
-  print("first commit");
+ int num1=10;
+ int num2=20;
+ if(num1 == num2){
+  print("if block working.. ");
+ }else{
+  print("else block working .. . ");
+}
 }
